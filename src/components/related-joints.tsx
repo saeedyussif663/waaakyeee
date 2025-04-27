92d6622f3c4bd7a009f592119cfed8f1f501fa23
saeedyussif663 <saeedyussif663@gmail.com>
2025-04-27 15:42:11 +0000

    update

--- src/components/related-joints.tsx
+++ src/components/related-joints.tsx
@@ -1,14 +1,12 @@
-import WaakyeCard from "./ui/waakye";
-
 export default function RelatedJoints() {
   return (
     <section className="my-10 w-full space-y-4">
       <h4 className="text-2xl font-semibold text-[#414141]">Related Joints</h4>
       <article className="mt-10 flex w-full flex-wrap items-center justify-center gap-6 sm:justify-start xl:gap-12">
+        {/* <WaakyeCard />
         <WaakyeCard />
         <WaakyeCard />
-        <WaakyeCard />
-        <WaakyeCard />
+        <WaakyeCard /> */}
       </article>
     </section>
   );
